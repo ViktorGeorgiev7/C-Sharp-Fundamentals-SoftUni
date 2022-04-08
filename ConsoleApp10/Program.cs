a4f0c22ff9f17f4c4ab1f9532af4cd6dd6b66444
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2022-04-09 00:49:59 +0300

    added-main

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //testtest1
+        }
+    }
+}
