f723a1ae48dde662ccdf3de53a05f0c229c5caae
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2022-04-19 20:22:36 +0300

    Added Arrays Exercises

---
+++ Arrays_Exercise/1,Arrays_Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1_Arrays_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] array = new int[count];
+
+            int sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+
+            }
+            Console.WriteLine(String.Join(' ', array));
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
---
+++ Arrays_Exercise/4_Arrays_Exercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _4_Arrays_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rotation = int.Parse(Console.ReadLine());
+
+            for (int rot = 0; rot < rotation; rot++)
+            {
+                int firstEl = arr[0];
+
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+                arr[arr.Length - 1] = firstEl;
+            }
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
