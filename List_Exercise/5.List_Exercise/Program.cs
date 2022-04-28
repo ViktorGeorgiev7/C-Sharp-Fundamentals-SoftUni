babcf2722fdef5a89cc220b132b761aa7ecbe67f
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2022-04-28 22:11:11 +0300

    Added List Exercises

---
+++ List_Exercise/5.List_Exercise/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] bombInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int bombNumber = bombInfo[0];
+            int bombPower = bombInfo[1];
+
+            while (true)
+            {
+
+                int indexOfBomb = list.IndexOf(bombNumber);
+
+                if (indexOfBomb == -1)
+                {
+                    break;
+                }
+                DetonateBomb(list, indexOfBomb, bombPower);
+            }
+            Console.WriteLine(list.Sum());
+        }
+        static void DetonateBomb(List<int> numbers, int indexOfBomb, int bombPower)
+        {
+            int rightCount = indexOfBomb + bombPower;
+
+            for (int cnt = indexOfBomb; cnt <= rightCount; cnt++)
+            {
+                if (indexOfBomb >= numbers.Count)
+                {
+                    break;
+                }
+                numbers.RemoveAt(indexOfBomb);
+            }
+
+            int leftCount = indexOfBomb - bombPower;
+
+            if (leftCount < 0)
+            {
+                leftCount = 0;
+            }
+            for (int cnt = leftCount; cnt < indexOfBomb; cnt++)
+            {
+                numbers.RemoveAt(leftCount);
+            }
+        }
+    }
+    }
+
---
+++ List_Exercise/7.List_Exercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.List_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToList();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<string> list = numbers[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int j = 0; j < list.Count; j++)
+                {
+                    Console.Write($"{list[j]} ");
+                }
+            }
+        }
+    }
+}
